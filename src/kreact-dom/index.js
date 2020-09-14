e926a225e66d547a948affea01bed41791231adc
张灿 <394203959@qq.com>
2020-09-14 11:28:15 +0800

    [day2] fiber structure has done

--- src/kreact-dom/index.js
+++ src/kreact-dom/index.js
@@ -1,4 +1,4 @@
-import {TEXT} from "../shared/const";
+import {PLACEMENT, TEXT} from "../shared/const";
 
 function createDOMAccordingToType(tagName, props) {
     let result;
@@ -6,14 +6,6 @@ function createDOMAccordingToType(tagName, props) {
         result = document.createTextNode('');
     } else if (typeof tagName === 'string') { // div, p, span 这些原生标签
         result = document.createElement(tagName);
-    } else if(typeof tagName === 'function') { // 有两种情况：class 组件和 function 组件
-        if(tagName.isReactComponent) { // class 组件
-            result = createNode(new tagName(props).render())
-        } else { // function 组件
-            result = createNode(tagName(props));
-        }
-    } else { // fragment 的情况
-        result = document.createDocumentFragment();
     }
     return result;
 }
@@ -30,17 +22,6 @@ function addAttributesToDOM(props, result) {
     }
 }
 
-function addChildrenToDOM(props, result) {
-    if(props && props.children) {
-        for(let child of props.children) {
-            render(child, result);
-            // 也可以也成下面这两步。最初没反应过来时，我就写了两步，其实一样的。render 就是对下面两步的封装。
-            // child = createNode(child);
-            // result.appendChild(child)
-        }
-    }
-}
-
 // 将 vnode 转为 真实 dom
 function createNode(vnode) {
     let result; // 即 node
@@ -52,17 +33,124 @@ function createNode(vnode) {
     // 2. 遍历属性，把属性添加到元素上
     addAttributesToDOM(props, result);
 
-
-    // 3. 遍历 children，
-    addChildrenToDOM(props,result);
-
     return result;
 }
 
+let wipRoot = null; // work in progress fiber
+let nextUnitOfWork = null; // fiber for loop
+
+// fiber 结构：
+// type: 类型
+// props:
+// key
+
+// child: 第一个孩子
+// sibling: 兄弟
+// return：父元素
+
+// node: 真实 dom 节点
+// base: 当前元素，本次没用到
+
 function render (vnode, element) {
-    const node = createNode(vnode);
-    element.appendChild(node);
+    // 初始化第一个 fiber 和用于循环的 fiber
+    wipRoot = {
+        props: {
+            children: [vnode]
+        },
+        node: element,
+        base: null
+    };
+
+    nextUnitOfWork = wipRoot;
 }
+
+// 遍历 children，为当前节点形成 fiber 架构
+function reconcileChildren(fiber, children) {
+    let prevSibling = null;
+    for (let i = 0; i < children.length; i++) {
+        let child = children[i];
+        let newFiber = {
+            type: child.type,
+            props: child.props,
+            node: null,
+            base: null,
+            return: fiber,
+            effectTag: PLACEMENT, // 注意有个 effectTag，整个对象要注意一下
+        };
+        if (i === 0 ) {
+            fiber.child = newFiber;
+        } else {
+            prevSibling.sibling = newFiber;
+        }
+        prevSibling = newFiber;
+    }
+}
+
+function updateClassComponent(fiber) {
+    const { type, props } = fiber;
+    const children = [(new type(props)).render()];
+    reconcileChildren(fiber, children);
+}
+
+function updateFunctionComponent(fiber) {
+    const { type, props } = fiber;
+    const children = [type(props)];
+    reconcileChildren(fiber, children);
+}
+
+function updateHostComponent(fiber) {
+    // 细节：fiber.node
+    if (!fiber.node) {
+        fiber.node = createNode(fiber); // 注意这里使用了 createNode
+    }
+    const { children } = fiber.props;
+    reconcileChildren(fiber, children)
+}
+
+function performUnitOfWork(fiber) {
+    // 1. 为当前 fiber 形成结构，或者说更新当前 fiber
+    const { type } = fiber;
+    if (typeof type === 'function') {
+        type.isReactComponent ? updateClassComponent(fiber) : updateFunctionComponent(fiber);
+    } else {
+        // 原生标签
+        updateHostComponent(fiber);
+    }
+
+    // 2. 返回下一个 fiber 用于执行
+    if (fiber.child) {
+        return fiber.child;
+    }
+
+    let nextFiber = fiber;
+    while(nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling;
+        }
+        nextFiber = nextFiber.return;
+    }
+}
+
+function commitRoot() {
+    console.log('xxxx', wipRoot)
+    // wipRoot = null;
+}
+
+function workLoop(idleDeadline) {
+    // 1. 形成 fiber 架构，注意这里有循环
+    while (nextUnitOfWork && idleDeadline.timeRemaining() > 0) {
+        nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    }
+
+    // 2. 根据 fiber 架构形成 dom 关系，渲染到页面上
+    // TODO，解释一下 !nextUnitOfWork
+
+    if (!nextUnitOfWork && wipRoot) {
+        commitRoot();
+    }
+}
+
+window.requestIdleCallback(workLoop);
 
 export default {
     render
